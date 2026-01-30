8554cdfaf5d79fb9d7f4cec31cb0e5cefdc83380
Thomas M <montigny.thomas@gmail.com>
2026-01-30 09:43:46 +0100

    feat: first version of the board, filled with random values

---
+++ memory-game.py
@@ -0,0 +1,34 @@
+# ---- Imports ----
+import random
+
+# ---- Constants ----
+# Board
+BOARD_HEIGHT = 4
+BOARD_WIDTH = 4
+NB_PAIRS = (BOARD_HEIGHT * BOARD_WIDTH) // 2
+
+board = [[-1 for _ in range(BOARD_WIDTH)] for _ in range(BOARD_HEIGHT)]
+
+pairs = [i for i in range(NB_PAIRS)] * 2
+#print(pairs)
+
+# ---- Functions ----
+def fill_board(board, pairs):
+    while not pairs == []:
+        x = random.randint(0, BOARD_WIDTH-1)
+        y = random.randint(0, BOARD_HEIGHT-1)
+
+        if board[x][y] == -1:
+            board[x][y] = pairs.pop()
+    
+    return board
+
+def print_board(board):
+    for line in board:
+        for value in line:
+            print(f"{value} ", end="")
+        print()
+
+# ---- Tests ----
+board = fill_board(board, pairs)
+print_board(board)
